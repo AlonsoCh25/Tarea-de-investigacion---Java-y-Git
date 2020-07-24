dd7950a80312e27140a4a6f8541fcead678b90e0
LuisDiego10 <66862458+LuisDiego10@users.noreply.github.com>
2020-07-24 10:16:47 -0600

    Funciones triggnométricas SENO, COSENO Y TANGENTE listas
    La calculadora cuenta con 3 opciones de cálculo (seno, coseno y tangente). Esto para cualquier ángulo que se desee calcular

--- Funcion Seno Coseno y tangente.java
+++ Funcion Seno Coseno y tangente.java
@@ -5,7 +5,8 @@ class MyClass {
         System.out.println("CALCULADORA DE FUNCIONES TRIGONOMÉTRICAS");
         System.out.println("Ingrese el ángulo que desea calcular:");
         Scanner angulo_ingresado = new Scanner(System.in); // Recibe el ángulo que desea calcular 
-        String angulo = angulo_ingresado.nextLine();
+        double angulo = angulo_ingresado.nextDouble();
+        double radianes=Math.toRadians(angulo);
         //OPCIONES DE LA CALCULADORA
         System.out.println("El angulo a calcular es: " + angulo);
         System.out.println("Tipo de función que desea calcular");
@@ -13,13 +14,21 @@ class MyClass {
         System.out.println("2. Calcular coseno");
         System.out.println("3. Calcular tangente");
         Scanner opcion = new Scanner(System.in);
-        String opcion_elegida = opcion.nextLine();
-        System.out.println("La opción elegida es: " + opcion_elegida);
-        //double radianes= Math.toRadians(angulo);
-        //double resultado=Math.sin(radianes);
-       // double resultado=0;
-        //angulo_num=angulo.nextDouble();
-        //double radianes=Math.toRadians(angulo_num)
-        //System.out.println("El resultado es: " + resultado);
+        int opcion_elegida = opcion.nextInt();        
+        if (opcion_elegida==1)
+        {
+        double resultado=Math.sin(radianes);
+        System.out.println("El resultado de calcular el seno de " + angulo + " es: " + resultado);
+        }
+        if (opcion_elegida==2)
+        {
+        double resultado=Math.cos(radianes);
+        System.out.println("El resultado de calcular el coseno de " + angulo + " es: " + resultado);
+        }
+        if (opcion_elegida==3)
+        {
+        double resultado=Math.tan(radianes);
+        System.out.println("El resultado de calcular la tangente de " + angulo + " es: " + resultado);
+        }
   }
 }
