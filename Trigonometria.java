8003c39be97eb18ec41c48aa88f24442a2f5d49c
LuisDiego10 <66862458+LuisDiego10@users.noreply.github.com>
2020-07-24 11:44:03 -0600

    Modificaciones sencillas
    Mostrar datos de tarea y mostrar angulo en resultado

--- Trigonometria.java
+++ Trigonometria.java
@@ -1,9 +1,20 @@
+/*
+            TALLER DE PROGRAMACIÓN I SEMESTRE 2020
+            TAREA DE INVESTIGACIÓN DE GIT HUB Y JAVA
+                
+                "CALCULADORA TRIGONOMÉTRICA"
+Integrantes:
+    Marco Gonzales 2020034547
+    Diego Garcia 2020124283
+    Kenneth Castillo 2019062984
+*/
 import java.util.Scanner;  // Import the Scanner class
 
 class Trigonometria {
   public static void main(String[] args) {
         //Se defienen las variables
         Scanner in = new Scanner(System.in);
+        double angle_g = 0;
         double angle = 0;
         int option_c = 0;
         int option_f = 0;
@@ -12,9 +23,9 @@ class Trigonometria {
         //Ingreso del Angulo
         System.out.println("CALCULADORA DE FUNCIONES TRIGONOMETRICAS");
         System.out.println("Ingrese el angulo que desea calcular:");
-        angle = in.nextInt();
+        angle_g = in.nextInt();
         //Convierte el angulo a radianes
-        angle = Math.toRadians(angle);
+        angle = Math.toRadians(angle_g);
 
         //Solicita el tipo de funcion que se desea calcular
         System.out.println("El angulo en radianes a calcular es: " + angle);
@@ -31,54 +42,54 @@ class Trigonometria {
             System.out.println("3. Funcion Tangente");
             option_f = in.nextInt();
             if(option_f == 1){
-                result = Math.sin(angle);;
-                System.out.println("El resultado de la Funcion Seno es:" + result);
+                result = Math.sin(angle);
+                System.out.println("El resultado de la Funcion Seno de " + angle_g + " es: " + result);
             }
             else if(option_f ==2){
                 result = Math.cos(angle);
-                System.out.println("El resultado de la Funcion Coseno es:" + result);
+                System.out.println("El resultado de la Funcion Coseno de: "+ angle_g + " es: " + result);
             }
             else if(option_f == 3){
                 result = Math.tan(angle);
-                System.out.println("El resultado de la Funcion Tangente es:" + result);
+                System.out.println("El resultado de la Funcion Tangente de: "+ angle_g + " es: " + result);
             }
         }
         if(option_c == 2){
-            System.out.println("ingrese la funcion que desea calcular");
+            System.out.println("Ingrese la funcion que desea calcular");
             System.out.println("1. Funcion Inversa de Seno");
             System.out.println("2. Funcion Inversa de Coseno");
             System.out.println("3. Funcion Inversa de Tangente");
             option_f = in.nextInt();
             if(option_f == 1){
-                result = Math.asin(angle);;
-                System.out.println("El resultado de la Funcion Inversa de Seno es:" + result);
+                result = Math.asin(angle);
+                System.out.println("El resultado de la Funcion Inversa de Seno de: "+ angle_g + " es: " + result);
             }
             else if(option_f ==2){
                 result = Math.acos(angle);
-                System.out.println("El resultado de la Funcion Inversa de Coseno es:" + result);
+                System.out.println("El resultado de la Funcion Inversa de Coseno de: "+ angle_g + " es:" + result);
             }
             else if(option_f == 3){
                 result = Math.atan(angle);
-                System.out.println("El resultado de la Funcion Inversa de Tangente es:" + result);
+                System.out.println("El resultado de la Funcion Inversa de Tangente de: "+ angle_g + " es:" + result);
             }
         }
         if(option_c == 3){
-            System.out.println("ingrese la funcion que desea calcular");
+            System.out.println("Ingrese la funcion que desea calcular");
             System.out.println("1. Funcion Hiperbolica de Seno");
             System.out.println("2. Funcion Hiperbolica de Coseno");
             System.out.println("3. Funcion Hiperbolica de Tangente");
             option_f = in.nextInt();
             if(option_f == 1){
                 result = Math.sinh(angle);;
-                System.out.println("El resultado de la Funcion Hiperbolica de Seno es:" + result);
+                System.out.println("El resultado de la Funcion Hiperbolica de Seno de: "+ angle_g + " es: " + result);
             }
             else if(option_f ==2){
                 result = Math.cosh(angle);
-                System.out.println("El resultado de la Funcion Hiperbolica de Coseno es:" + result);
+                System.out.println("El resultado de la Funcion Hiperbolica de Coseno de: "+ angle_g + " es: " + result);
             }
             else if(option_f == 3){
                 result = Math.tanh(angle);
-                System.out.println("El resultado de la Funcion Hiperbolica de Tangente es:" + result);
+                System.out.println("El resultado de la Funcion Hiperbolica de Tangente de: "+ angle_g + " es: " + result);
             }
         }    
   }
